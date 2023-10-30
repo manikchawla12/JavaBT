8a931db12afde5ab95949bc3a4bf5b5657f779d4
Manik Chawla <69040146+manikchawla12@users.noreply.github.com>
2023-10-30 09:51:33 +0530

    Add files via upload

--- testsApi/testsApi/src/main/java/com/health/controller/TestsController.java
+++ testsApi/testsApi/src/main/java/com/health/controller/TestsController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,9 @@ import com.health.model.Tests;
 import com.health.testsRepository.TestsRepository;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
+
 @RequestMapping("/")
 public class TestsController {
 	
@@ -38,20 +42,20 @@ public class TestsController {
 		return new ResponseEntity<List<Tests>>(testList,HttpStatus.OK);
 	}
 	
-	@GetMapping("/tests/{testsId}")
-	public ResponseEntity<Tests>getTestById(@PathVariable long testsId){
-		Optional<Tests> test=testsRepository.findById(testsId);
-		if(test.isPresent()) {
-			return new ResponseEntity<Tests>(test.get(),HttpStatus.OK);
-		}
-		else {
-			return new ResponseEntity<Tests>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	@DeleteMapping("/tests/{testsId}")
-	public String deleteTestById(@PathVariable long testsId) {
-		testsRepository.deleteById(testsId);
+//	@GetMapping("/tests/{id}")
+//	public ResponseEntity<Tests>getTestById(@PathVariable long id){
+//		Optional<Tests> test=testsRepository.findById(id);
+//		if(test.isPresent()) {
+//			return new ResponseEntity<Tests>(test.get(),HttpStatus.OK);
+//		}
+//		else {
+//			return new ResponseEntity<Tests>(HttpStatus.NOT_FOUND);
+//		}
+//	}
+//	
+	@DeleteMapping("/tests/{id}")
+	public String deleteTestById(@PathVariable long id) {
+		testsRepository.deleteById(id);
 		return "Test removed";
 	}
 	
